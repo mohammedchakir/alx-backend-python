8e2c188d5bc92976fdb2399088359f50a3901de6
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-24 18:34:11 +0100

    add cpmmit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ Unit tests for utils.get_json function.
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import get_json, access_nested_map
+from utils import get_json, access_nested_map, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -62,6 +62,32 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test case for the memoize decorator in utils module.
+    """
+
+    def test_memoize(self):
+        """
+        Test that memoize decorator caches the result of a method.
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test_instance = TestClass()
+            first_call = test_instance.a_property
+            second_call = test_instance.a_property
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
