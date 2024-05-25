26a78a2ff8010120f690ed76eef4ea7e190d40c7
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-25 21:42:15 +0100

    add changes

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -106,18 +106,19 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False)
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({"license": None}, "my_license", False),
+        ({}, "my_license", False)
     ])
-    def test_has_license(self, repo, license_key, expected_result,
-                         mock_get_json):
+    def test_has_license(self, repo: dict, license_key: str,
+                         expected: bool) -> None:
         """
-        Test GithubOrgClient.has_license returns the correct boolean value.
+        Test that GithubOrgClient.has_license returns the correct
+        boolean value.
         """
-        mock_get_json.return_value = [repo]
         client = GithubOrgClient("test_org")
-        result = client.has_license(license_key)
-        self.assertEqual(result, expected_result)
-        mock_get_json.assert_called_once()
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
