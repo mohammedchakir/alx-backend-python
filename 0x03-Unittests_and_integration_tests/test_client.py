5df903cc106c77e31dced5f18148cb4d2ca03635
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-25 01:43:51 +0100

    aff for check

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Unit tests for client.GithubOrgClient class.
 """
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -30,6 +30,20 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(result, mock_get_json.return_value)
         mock_get_json.assert_called_once_with(expected_url)
+
+    def test_public_repos_url(self):
+        """
+        Test that GithubOrgClient._public_repos_url returns the correct URL.
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            org_payload = {"public_repos": 5}
+            mock_org.return_value = org_payload
+            client = GithubOrgClient("test_org")
+            result = client._public_repos_url
+            expected_result = f"https://api.github.com/orgs/test_org/repos? /\
+                per_page={org_payload['public_repos']}&page=1"
+            self.assertEqual(result, expected_result)
 
 
 if __name__ == "__main__":
